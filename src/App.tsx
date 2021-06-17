2799cc40f7ce153633e65745e75a3053f3267d2f
Piotr Adamczyk <xeno@xeno.yt>
2021-06-17 22:50:45 +0200

    workaround for half-drawn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React, { ReactEventHandler } from "react";
+import React, { ReactEventHandler, useEffect, useState } from "react";
 
 import TopBar from "./components/TopBar";
 
 import styles from "./App.module.css";
+import Spinner from "./components/Spinner";
 
 export default function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [shown, setShown] = useState(false);
+
+  useEffect(() => {
+    if (loaded && !shown) {
+      // Workaround for half-drawn UI
+      const timeout = setTimeout(() => {
+        setShown(true);
+      }, 2000);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+    if (!loaded && shown) {
+      setShown(false);
+    }
+  }, [loaded, shown]);
+
   const onLoad: ReactEventHandler<HTMLIFrameElement> = (e) => {
     const frame = e.currentTarget;
-    setTimeout(() => {
-      frame.contentDocument.head.innerHTML += `<style>${window.customAppleMusicCss}</style>`;
-    }, 1000);
+    frame.contentDocument.head.innerHTML += `<style>${window.customAppleMusicCss}</style>`;
+    setLoaded(true);
+  };
+  const onLoadStart: ReactEventHandler<HTMLIFrameElement> = (e) => {
+    setLoaded(false);
   };
   return (
     <div className={styles.container}>
       <TopBar />
       <iframe
+        style={{ display: shown ? "block" : "none" }}
         src="https://music.apple.com/library/recently-added"
         className={styles.webview}
         onLoad={onLoad}
+        onLoadStart={onLoadStart}
         allow="encrypted-media *;"
       />
+      {!shown && <Spinner />}
     </div>
   );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "./Spinner.module.css";
+
+export default function Spinner() {
+  return (
+    <div className={styles.spinner}>
+      <div className={styles.bar1}></div>
+      <div className={styles.bar2}></div>
+      <div className={styles.bar3}></div>
+      <div className={styles.bar4}></div>
+      <div className={styles.bar5}></div>
+      <div className={styles.bar6}></div>
+      <div className={styles.bar7}></div>
+      <div className={styles.bar8}></div>
+      <div className={styles.bar9}></div>
+      <div className={styles.bar10}></div>
+      <div className={styles.bar11}></div>
+      <div className={styles.bar12}></div>
+    </div>
+  );
+}
